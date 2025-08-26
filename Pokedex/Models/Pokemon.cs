2b9fed73bb03e7800e97acfd14fdfab612f79f4a
Tainara <tainaravit.santos28@gmail.com>
2025-08-25 22:36:29 -0300

    Iniciando o projeto e a camada de dados

---
+++ Pokedex/Models/Pokemon.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
+
+namespace Pokedex.Models;
+    [Table("Pokemon")]
+    public class Pokemon
+    {
+       [Key]
+       [DatabaseGenerated(DatabaseGeneratedOption.None)]
+       public uint  Numero { get; set; }
+
+      [Required(ErrorMessage = "Por favor, informe a regiao")]
+      public uint RegiaoId { get; set; }
+      [ForeignKey("RegiaoId")]
+      public Regiao Regiao { get; set; } //propriedade de navegação
+
+     [Required(ErrorMessage = "Por favor, informe o Gênero")]
+     public uint GeneroId { get; set; }
+     [ForeignKey("GeneroId")]
+     public Genero Genero { get; set; }
+
+     [Required(ErrorMessage = "Por favor, informe o nome")]
+     public string Nome { get; set; }
+
+     [StringLength(1000)]
+     public string Descricao { get; set; }
+
+    [Required(ErrorMessage = "Informe a altura")]
+    [Column(TypeName = "double(5,2)")] // especifica a altura
+     public double Altura { get; set; } //doble é um numero grande
+
+      [Required(ErrorMessage = "Informe o peso")]
+    [Column(TypeName = "double(7,3)")] // especifica a altura
+     public double Peso { get; set; } //doble é um numero grande
+
+    }
---
+++ Pokedex/Models/Tipo.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pokedex.Models;
+[Table("Tipo")]
+
+    public class Tipo
+    {
+        [Key] // chave primaria da minha tabela
+        public uint Id { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe o nome")]
+        [StringLength(30)]
+        public string Nome { get; set; } //unique so pra ter um nome deste tipo
+
+        [StringLength(25)]
+        public string Cor { get; set; }
+    }
